4e859583881b1f111940f94891e983b1278fb05e
KhusnutdinovRustem <167807173+KhusnutdinovRustem@users.noreply.github.com>
2024-04-24 00:02:17 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,7 @@
+name = 'Khusnutdinov Rustem'
+print(name[0])
+print(name[-1])
+print(name[2:5])
+print(name[::-1])
+print(len(name)) #этого кстати в лекции не было
+print('это новая строка ' + 'это моя строка') #последнее задание я понял так
